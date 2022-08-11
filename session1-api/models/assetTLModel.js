11f9c53a8c9f5a730761b8ad8bc3d12956510fc5
iMAC 16 MAC Lab <imac16maclab@net-130-25-3-148.cust.vodafonedsl.it>
2022-08-11 14:56:41 +0800

    Modified the assetTLModel

--- session1-api/models/assetTLModel.js
+++ session1-api/models/assetTLModel.js
@@ -1,7 +1,7 @@
 const conn = require("../db_config");
 
 const getAssetTransferLogs = (result) => {
-  sqlQuery = "SELECT * FROM assettransferlogs";
+  sqlQuery = "SElECT `atl`.`ID`, `a`.`AssetName`, `atl`.`FromAssetSN`, `atl`.`ToAssetSN`, `df`.`Name` AS 'FromDepartment', `dm`.`Name` AS 'ToDepartment', `atl`.`TransferDate` FROM assettransferlogs `atl` LEFT JOIN assets `a` ON `a`.`ID` = `atl`.`AssetID` LEFT JOIN departmentlocations `dlf` ON `atl`.`FromDepartmentLocationID` = `dlf`.`ID` LEFT JOIN departmentlocations `dlt` ON `atl`.`ToDepartmentLocationID` = `dlt`.`ID` LEFT JOIN departments `df` ON `dlf`.`DepartmentID` = `df`.`ID` LEFT JOIN departments `dm` ON `dlt`.`DepartmentID` = `dm`.`ID`;";
   conn.query(sqlQuery, (err, res) => {
     if (err) {
       console.log("Cannot fetched assettransferlogs", err);
